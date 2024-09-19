246a60cd941d195dff916ef502b6543c4e91179a
Lin He <xlhe@cs.washington.edu>
2024-09-19 16:57:38 -0700

    change color button

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,2 +1,6 @@
 
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Lin He";
+document.querySelector("li a").style.color = "red";
+
+// change the button to a different color
+document.querySelector("button").style.backgroundColor = "yellow";
